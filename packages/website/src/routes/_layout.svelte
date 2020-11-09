f3e1307f8064e34631f68163348cf65498741563
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-11-09 14:55:45 +0100

    feat(website): added dev proxy

--- packages/website/src/routes/_layout.svelte
+++ packages/website/src/routes/_layout.svelte
@@ -5,14 +5,6 @@
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
 </style>
 
 <Nav {segment}/>
--- packages/website/src/server.ts
+++ packages/website/src/server.ts
@@ -1,17 +1,24 @@
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    '/api/graphql',
+    dev
+      ? createProxyMiddleware('http://localhost:8888/api/graphql')
+      : (_req, _res, next) => next()
+  )
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
