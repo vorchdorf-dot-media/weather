3c2520a84bd6440b004720ca9e0e9313663767ad
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2021-05-01 17:15:19 +0200

    chore: fixed preview URL in deploy workflow

--- packages/website/utils/constants.ts
+++ packages/website/utils/constants.ts
@@ -1,7 +1,7 @@
 export const DAY = 86400000;
 
 export const getUrl = (): string => {
-  const fallback = process.env.DEPLOY_URL ?? 'http://localhost:3000';
+  const fallback = process.env.PREVIEW_URL ?? 'http://localhost:3000';
   if (process.env.CONTEXT === 'production' && process.env.PROD === 'true') {
     return process.env.URL ?? fallback;
   }
